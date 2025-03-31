275e695e693aeb6df7b9a5d7c1b782666ef5273e
Your Name <sm4442782@gmail.com>
2025-03-31 13:00:28 +0530

    Adding area_of_shapes.js

---
+++ area_of_shapes.js
@@ -0,0 +1,20 @@
+let shapes = {
+  square : function (a){
+    return a*a
+  },
+  rectangle : function (l,b){
+    return 2*(l+b)
+  },
+  triange : function (b,h){
+    return 1/2*b*h
+  },
+  circle : function (r){
+    return Number((Math.PI*r*r).toFixed(2))
+  },
+  cylinder : function (r,h){
+    return Number(((2*Math.PI*r)*(r+h)).toFixed(2))
+  },
+  cone : function(r,h){
+    return Number(((Math.PI*r)*(r+(Math.sqrt((h*h)+(r*r))))).toFixed(2))
+  }
+}
