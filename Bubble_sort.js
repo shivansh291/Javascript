186fab9b29ca430aa918a5e59fb8924508a5aad0
Your Name <sm4442782@gmail.com>
2025-04-03 11:07:15 +0530

    Adding bubble_sort.js

---
+++ Bubble_sort.js
@@ -0,0 +1,13 @@
+let array = [5, 4, 3, 2, 1];
+
+for (let i = 0; i < array.length - 1; i++) {
+  for (let j = 0; j < array.length - 1; j++) {
+    if (array[j] > array[j + 1]) {
+      let temp = array[j];
+      array[j] = array[j + 1];
+      array[j + 1] = temp;
+    }
+  }
+}
+
+console.log(array);
